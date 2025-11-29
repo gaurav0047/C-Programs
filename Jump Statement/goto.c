e5d0bb9f50d399cdc313f660e1977abbd934d03c
gaurav0047 <gauravpatil141099@gmail.com>
2025-11-29 23:09:23 +0530

    goto ste5

---
+++ Jump Statement/goto.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// Jump Statement goto
+int main(void)
+{
+  printf("Line-1\n");
+  goto TEST;
+  printf("Line-2\n");
+  printf("Line-3\n");
+TEST:
+  printf("Line-4\n");
+  printf("Line-5\n");
+  return 0;
+}
