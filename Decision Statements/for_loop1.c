d3cabb637fccdf674eab9c4dbb172c8dba505b84
gaurav0047 <gauravpatil141099@gmail.com>
2025-11-29 23:03:47 +0530

    For Loop 1

---
+++ Decision Statements/for_loop1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+  int cnt;
+  printf("START\n");
+  for (cnt = 1; cnt <= 10; ++cnt)
+  {
+    printf("Hello FYBCA - %d \n", cnt);
+  }
+  printf("END\n");
+  return 0;
+}
