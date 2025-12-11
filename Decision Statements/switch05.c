c7277bb4b2d4f5f106cf4a103786a919e1264dfa
gaurav0047 <gauravpatil141099@gmail.com>
2025-12-11 17:32:06 +0530

    Working of switch 5

---
+++ Decision Statements/switch05.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	
+	int k;
+	float j=2.0;
+	
+	switch(k=j+1)  
+	{
+	    case 3: printf("\nTrapped");break;
+	    
+	    default: printf("\nSayonara");
+	
+	}
+		
+	return 0;
+	
+}
