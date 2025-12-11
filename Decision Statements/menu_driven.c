536d7d130b3dc7a239952529542171bfad4f4857
gaurav0047 <gauravpatil141099@gmail.com>
2025-12-11 17:47:14 +0530

    Menu driven C program using switch

---
+++ Decision Statements/menu_driven.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  int num1, num2, ans, choice;
+  char wish;
+  printf("Enter First No.::");
+  scanf("%d", &num1);
+  printf("\nEnter Second No.::");
+  scanf("%d", &num2);
+
+  do
+  {
+    system("cls");
+    printf("\n=====================================\n");
+    printf("        Arithmetic Operations Menu     \n");
+    printf("=====================================\n");
+    printf("Current Numbers: num1 = %d , num2 = %d\n", num1, num2);
+    printf("-------------------------------------\n");
+    printf("\n::MENU::\n");
+    printf("1.ADD\n2.SUB\n3.MUL\n4.DIV\n5.MOD\n6.EXIT\n");
+
+    printf("\nENTER YOUR CHOICE ::");
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1:
+      ans = num1 + num2;
+      printf("\n %d + %d = %d \n", num1, num2, ans);
+      break;
+    case 2:
+      ans = num1 - num2;
+      printf("\n %d - %d = %d \n", num1, num2, ans);
+      break;
+    case 3:
+      ans = num1 * num2;
+      printf("\n %d * %d = %d \n", num1, num2, ans);
+      break;
+    case 4:
+      ans = num1 / num2;
+      printf("\n %d / %d = %d \n", num1, num2, ans);
+      break;
+    case 5:
+      ans = num1 % num2;
+      printf("\n %d %% %d = %d \n", num1, num2, ans);
+      break;
+    case 6:
+      printf("\nThank you. You are exiting the application");
+      exit(1);
+    default:
+      printf("\nInvalid choice given.\n");
+    }
+    printf("Do you want to continue(y/Y) ?::");
+    fflush(stdin);
+    wish = getchar();
+
+  } while (wish == 'y' || wish == 'Y');
+
+  printf("END\n");
+  return 0;
+}
