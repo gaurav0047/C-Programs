2fc81565075eae71ce8d4af8bcea020984b250a1
gaurav0047 <gauravpatil141099@gmail.com>
2025-12-11 17:27:57 +0530

    Working of switch 3

---
+++ Decision Statements/switch03.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	
+	int i=3;
+	
+	switch(i)
+	{
+	    case 0:   printf("\nPune");
+	    case 1+0: printf("\nThane");
+	    case 4/2: printf("\nMumbai");
+	    case 8%5: printf("\nSatara");
+	}
+	
+	return 0;
+	
+}
