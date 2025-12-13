a2785230da025770296a2116fd3668336d3855c4
gaurav0047 <gauravpatil141099@gmail.com>
2025-12-13 23:13:28 +0530

    Array2

---
+++ Array/array2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+	int arr[5];
+	arr[2] = 45;
+	arr[0] = 100;
+	
+	printf("Value \t Address \n");
+	printf("======================\n");
+	printf("%d \t %u \n", arr[0], &arr[0]);
+	printf("%d \t %u \n", arr[1], &arr[1]);
+	printf("%d \t %u \n", arr[2], &arr[2]);
+	printf("%d \t %u \n", arr[3], &arr[3]);
+	printf("%d \t %u \n", arr[4], &arr[4]);
+ 
+	return 0;
+}
