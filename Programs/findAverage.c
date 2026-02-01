30e3afb7d9947e52ade92395696d361e21088d97
gaurav0047 <gauravpatil141099@gmail.com>
2026-02-01 16:33:20 +0530

    Average of Numbers

---
+++ Programs/findAverage.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+//Program to calculate and print average of 3 integers  
+
+int main()
+{
+  
+ 
+    /*int ans = (10+12+6)/3;
+	printf("answer = %d \n", ans); 	*/
+	
+	/*float ans = (10+12+6)/3;
+	printf("answer = %f \n", ans); 	*/
+	
+	/*float ans = (10+12+6)/3.0f;
+	printf("answer = %f \n", ans); */
+	
+	//float ans = (10+12+6)/(float)3;
+	//float ans = (float)(10+12+6)/3;
+	
+	float ans = (10 + (float)12 + 6)/3;
+	printf("answer = %f \n", ans);
+
+    return 0;
+}
