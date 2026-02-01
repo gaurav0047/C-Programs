7c6edebb95ca0b85c84c47fc6cacc66fe2fcf2a0
gaurav0047 <gauravpatil141099@gmail.com>
2026-02-01 16:17:59 +0530

    Printing Hello in different ways

---
+++ Programs/print_hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>  
+ int main()    
+{ 
+  // Using if statement   
+ if(printf("Hello world")){}  
+ 
+ //Using switch statemet 
+ switch(printf("\nHello world")){}    
+
+ //Using while
+while(!printf("\nHello world")){}  
+return 0;  
+}   
