84c118453c3f8dd73a4c9059425a834ad8202dc9
gaurav0047 <gauravpatil141099@gmail.com>
2026-02-01 16:03:47 +0530

    Character

---
+++ Programs/type_modifier1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <limits.h>
+// char data type
+int main()
+{
+
+  char ch = -130; // 125, -125, 136, 255 etc.
+
+  printf("Value of  ch = %d \n", ch);
+
+  return 0;
+}
