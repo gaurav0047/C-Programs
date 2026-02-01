251519a7b3ea7d1dbe23219a366226d2240cde06
gaurav0047 <gauravpatil141099@gmail.com>
2026-02-01 16:36:47 +0530

    Format Specifiers and representations of the numbers

---
+++ Programs/number_system.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+//Format Specifiers and representations of the numbers 
+
+int main()
+{
+	int a = 24;
+	
+	int b = 024;
+	
+	int c = 0x24;
+	
+	int d = 0b10101;
+	
+	printf("a = %d\n",a);
+	
+	printf("b = %d\n",b);
+	
+	printf("c = %d\n",c);
+	
+	printf("d = %d\n",d);
+	  	  	
+ 	
+   return 0;
+}
