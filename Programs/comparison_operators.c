a6b109c5e127cce19c4c137820e990282edc8329
gaurav0047 <gauravpatil141099@gmail.com>
2026-02-01 16:02:14 +0530

    Comparison Operators

---
+++ Programs/comparison_operators.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/* Comparision/Relational Operators */
+
+int main()
+{
+  int a = 10, b = 2, c = 0;
+
+  printf("a<b == %d \n", a < b);
+  printf("a>b == %d \n", a > b);
+  printf("a==b == %d \n", a == b);
+  printf("a<=b == %d \n", a <= b);
+  printf("a>=b == %d \n", a >= b);
+  printf("a!=b == %d \n", a != b);
+
+  return 0;
+}
